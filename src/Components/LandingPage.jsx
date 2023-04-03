3255dc3c0619d11ec6e9e243a01aca948b794e21
MoinuddinZaheer <mueenuddinzaheer@gmail.com>
2023-04-03 11:13:17 +0530

    added links to landing page

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,9 +1,12 @@
+import { Link } from 'react-router-dom'
 import React from 'react'
 
 const LandingPage=() => {
   return (
     <div className='LandingPage'>
       <h1>login as...</h1>
+      <Link to="/admin-login">Admin Login</Link>
+      <Link to="/user-login">User Login</Link>
     </div>
   )
 }
